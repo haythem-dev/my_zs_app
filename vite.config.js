60d3991cc070f6e4ad9024c8205da93849758d3a
haythem-dev <149179174+haythem-dev@users.noreply.github.com>
2025-03-15 11:31:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'my-zs-app-1.onrender.com',
+      '.onrender.com' // Ceci permettra tous les sous-domaines onrender.com
+    ]
+  },
+  server: {
+    host: true, // Nécessaire pour Render
+    port: process.env.PORT || 3000,
+  }
 })
